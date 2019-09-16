461a8bdf5b2731c6602538ac8daa26e77c455bd2
Luiz Fernando Vital <luizfvital@gmail.com>
2019-09-16 00:08:39 -0300

    Initial commit

---
+++ public/js/chat.js
@@ -0,0 +1,24 @@
+const socket = io()
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+document.querySelector('#messageForm').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const message = e.target.elements.message.value
+    socket.emit('sendMessage', message)
+
+})
+
+document.querySelector('#sendLocation').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords
+        socket.emit('sendLocation', { latitude, longitude })
+    })
+})
